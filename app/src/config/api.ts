0276c75be2e8354ffa7b0638d714c8e10e220a34
adlljvd <adella.javd@gmail.com>
2025-01-16 00:10:15 +0700

    feat: api

--- app/src/config/api.ts
+++ app/src/config/api.ts
@@ -1,3 +1,1 @@
-// export const API_URL = "https://to-do-list.adellajava.tech";
-export const API_URL =
-  "https://a54d-2a02-ce0-1802-894d-aee5-cbd0-b0fd-6083.ngrok-free.app";
+export const API_URL = "http://todolist.adellajava.tech";
